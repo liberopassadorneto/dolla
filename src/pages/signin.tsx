581d1792f6f035f3ef4de8874b30cc2edd4bfe3e
Libero Passador Neto <libero@griks.com.br>
2021-11-04 20:15:03 -0400

    first commit

---
+++ src/pages/signin.tsx
@@ -0,0 +1,11 @@
+import { ScrollToTop } from '../components/ScrollToTop';
+import { Signin } from '../components/Signin';
+
+export function SigninPage() {
+  return (
+    <>
+      <ScrollToTop />
+      <Signin />
+    </>
+  );
+}
